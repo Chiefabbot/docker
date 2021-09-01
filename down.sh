b0719f516e9fe6da9986ab658c3d04af7054f851
Роман <garrysama@yandex.ru>
2021-09-01 12:47:02 +0000

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f efk.compose.yml -p rbmdkrfinalefk down && docker-compose -f app.compose.yml -p rbmdkrfinalapp down
+docker-compose -f efk.compose.yml -p rbmdkrfinalefk down && docker-compose -f app.compose.yml -p rbmdkrfinalapp down && docker network rm app-net && rm -rf caddy_data
